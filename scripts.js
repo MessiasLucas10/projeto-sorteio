64013995b24fc09cbfdb89e117f176976a73e8f9
Messiaslucas10 <messiaslucas67@gmail.com>
2024-03-20 20:25:19 -0300

    Alteração-script

--- scripts.js
+++ scripts.js
@@ -3,7 +3,13 @@ function generateNumber() {
     const min = Math.ceil(document.querySelector('.input-min').value)
     const max = Math.floor(document.querySelector('.input-max').value)
 
-    const result = Math.floor(Math.random() * (max - min + 1)) + min;
+    if(min >= max) {
+        alert("O primeiro número não pode ser maior que o segundo!")
+    } else {
+        const result = Math.floor(Math.random() * (max - min + 1)) + min;
     
     alert(result)
 }
+
+}
+
